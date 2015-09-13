1de8c4a0d05a7b6cba66467792363e50ceede996
wilson888888888 <wilson888888888@gmail.com>
2015-09-13 14:39:51 -0700

    fixed php again

--- sub.php
+++ sub.php
@@ -3,7 +3,7 @@ $name = $_REQUEST['name'];
 $project = $_REQUEST['project'];
 $file = fopen("projectlist.txt","a+");
 fwrite($file,$name);
-fwrite($file,$email);
+fwrite($file,$project);
 fwrite($file,"\r\n");
 fclose($file);
 print_r(error_get_last());
