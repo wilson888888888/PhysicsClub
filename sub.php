710373cc47059f5b19f2b5e95132a28eade730df
wilson888888888 <wilson888888888@gmail.com>
2015-09-13 00:26:45 -0700

    fixed typo in php

--- sub.php
+++ sub.php
@@ -4,5 +4,7 @@ $project = $_REQUEST['project'];
 $file = fopen("projectlist.txt","a+");
 fwrite($file,$name);
 fwrite($file,$email);
-fwrite($file,"\r\n")
+fwrite($file,"\r\n");
+fclose($file);
 print_r(error_get_last());
+?>
